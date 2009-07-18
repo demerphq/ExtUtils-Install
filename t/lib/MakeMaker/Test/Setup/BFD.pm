206257fe2d22487b9da8c9b2cfeb6fc7ada6ee8f
Yves Orton <demerphq@gmail.com>
2009-07-18 02:22:33 +0200

    sync with blead EUI 1.52_01 and other changes (particularly tests)

--- t/lib/MakeMaker/Test/Setup/BFD.pm
+++ t/lib/MakeMaker/Test/Setup/BFD.pm
@@ -11,96 +11,87 @@ use MakeMaker::Test::Utils;
 
 my $Is_VMS = $^O eq 'VMS';
 
-sub clean { my $str=shift; $str=~s/^##//mg; $str }
-
 my %Files = (
-             'Big-Dummy/Makefile.PL'          => clean(<<'END'),
-##use ExtUtils::MakeMaker;
-##
-### This will interfere with the PREREQ_PRINT tests.
-##printf "Current package is: %s\n", __PACKAGE__ unless "@ARGV" =~ /PREREQ/;
-##
-##WriteMakefile(
-##    NAME          => 'Big::Dummy',
-##    VERSION_FROM  => 'lib/Big/Dummy.pm',
-##    EXE_FILES     => [qw(bin/program)],
-##    PREREQ_PM     => { strict => 0 },
-##    ABSTRACT_FROM => 'lib/Big/Dummy.pm',
-##    AUTHOR        => 'Michael G Schwern <schwern@pobox.com>',
-##);
+             'Big-Dummy/lib/Big/Dummy.pm'     => <<'END',
+package Big::Dummy;
+
+$VERSION = 0.01;
+
+=head1 NAME
+
+Big::Dummy - Try "our" hot dog's
+
+=cut
+
+1;
 END
 
-             'Big-Dummy/INSTALL.SKIP'     => clean(<<'END'),
-##\.SKIP$
+             'Big-Dummy/Makefile.PL'          => <<'END',
+use ExtUtils::MakeMaker;
+
+# This will interfere with the PREREQ_PRINT tests.
+printf "Current package is: %s\n", __PACKAGE__ unless "@ARGV" =~ /PREREQ/;
+
+WriteMakefile(
+    NAME          => 'Big::Dummy',
+    VERSION_FROM  => 'lib/Big/Dummy.pm',
+    EXE_FILES     => [qw(bin/program)],
+    PREREQ_PM     => { strict => 0 },
+    ABSTRACT_FROM => 'lib/Big/Dummy.pm',
+    AUTHOR        => 'Michael G Schwern <schwern@pobox.com>',
+);
 END
 
-             'Big-Dummy/bin/program'          => clean(<<'END'),
-###!/usr/bin/perl -w
-##
-##=head1 NAME
-##
-##program - this is a program
-##
-##=cut
-##
-##1;
+             'Big-Dummy/bin/program'          => <<'END',
+#!/usr/bin/perl -w
+
+=head1 NAME
+
+program - this is a program
+
+=cut
+
+1;
 END
 
-             'Big-Dummy/lib/Big/Dummy.pm'     => clean(<<'END'),
-##package Big::Dummy;
-##
-##$VERSION = 0.01;
-##
-##=head1 NAME
-##
-##Big::Dummy - Try "our" hot dog's
-##
-##=cut
-##
-##1;
+             'Big-Dummy/t/compile.t'          => <<'END',
+print "1..2\n";
+
+print eval "use Big::Dummy; 1;" ? "ok 1\n" : "not ok 1\n";
+print "ok 2 - TEST_VERBOSE\n";
 END
 
-             'Big-Dummy/lib/Big/Dummy.SKIP'     => clean(<<'END'),
-##Skip me please!
+             'Big-Dummy/Liar/t/sanity.t'      => <<'END',
+print "1..3\n";
+
+print eval "use Big::Dummy; 1;" ? "ok 1\n" : "not ok 1\n";
+print eval "use Big::Liar; 1;" ? "ok 2\n" : "not ok 2\n";
+print "ok 3 - TEST_VERBOSE\n";
 END
 
-             'Big-Dummy/t/compile.t'          => clean(<<'END'),
-##print "1..2\n";
-##
-##print eval "use Big::Dummy; 1;" ? "ok 1\n" : "not ok 1\n";
-##print "ok 2 - TEST_VERBOSE\n";
+             'Big-Dummy/Liar/lib/Big/Liar.pm' => <<'END',
+package Big::Liar;
+
+$VERSION = 0.01;
+
+1;
 END
 
-             'Big-Dummy/Liar/Makefile.PL'     => clean(<<'END'),
-##use ExtUtils::MakeMaker;
-##
-##my $mm = WriteMakefile(
-##              NAME => 'Big::Liar',
-##              VERSION_FROM => 'lib/Big/Liar.pm',
-##              _KEEP_AFTER_FLUSH => 1
-##             );
-##
-##print "Big::Liar's vars\n";
-##foreach my $key (qw(INST_LIB INST_ARCHLIB)) {
-##    print "$key = $mm->{$key}\n";
-##}
+             'Big-Dummy/Liar/Makefile.PL'     => <<'END',
+use ExtUtils::MakeMaker;
+
+my $mm = WriteMakefile(
+              NAME => 'Big::Liar',
+              VERSION_FROM => 'lib/Big/Liar.pm',
+              _KEEP_AFTER_FLUSH => 1
+             );
+
+print "Big::Liar's vars\n";
+foreach my $key (qw(INST_LIB INST_ARCHLIB)) {
+    print "$key = $mm->{$key}\n";
+}
 END
 
-             'Big-Dummy/Liar/lib/Big/Liar.pm' => clean(<<'END'),
-##package Big::Liar;
-##
-##$VERSION = 0.01;
-##
-##1;
-END
-
-             'Big-Dummy/Liar/t/sanity.t'      => clean(<<'END'),
-##print "1..3\n";
-##
-##print eval "use Big::Dummy; 1;" ? "ok 1\n" : "not ok 1\n";
-##print eval "use Big::Liar; 1;" ? "ok 2\n" : "not ok 2\n";
-##print "ok 3 - TEST_VERBOSE\n";
-END
             );
 
 
@@ -117,12 +108,17 @@ sub setup_recurs {
         open(FILE, ">$file") || die "Can't create $file: $!";
         print FILE $text;
         close FILE;
+
+        # ensure file at least 1 second old for makes that assume
+        # files with the same time are out of date.
+        my $time = calibrate_mtime();
+        utime $time, $time - 1, $file;
     }
 
     return 1;
 }
 
-sub teardown_recurs {
+sub teardown_recurs { 
     foreach my $file (keys %Files) {
         my $dir = dirname($file);
         if( -e $dir ) {
