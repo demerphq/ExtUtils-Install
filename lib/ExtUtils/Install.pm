1cfeff18f2615d2a4c59ab8dfc60748df7e4ee05
Yves Orton <demerphq@gmail.com>
2009-07-27 23:41:02 +0200

    bump to 1.54 so we can rename MakeMaker.t to InstallWithMM.t

--- lib/ExtUtils/Install.pm
+++ lib/ExtUtils/Install.pm
@@ -38,11 +38,11 @@ ExtUtils::Install - install files from here to there
 
 =head1 VERSION
 
-1.53
+1.54
 
 =cut
 
-$VERSION = '1.53';  # <---- dont forget to update the POD section just above this line!
+$VERSION = '1.54';  # <---- dont forget to update the POD section just above this line!
 $VERSION = eval $VERSION;
 
 =pod
