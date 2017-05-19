56e5f0cd239059e3b3eacf8d799317e9fb286c2b
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2017-05-19 14:30:47 +0100

    Releasgineering for 2.12

--- lib/ExtUtils/Install.pm
+++ lib/ExtUtils/Install.pm
@@ -32,11 +32,11 @@ ExtUtils::Install - install files from here to there
 
 =head1 VERSION
 
-2.10
+2.12
 
 =cut
 
-our $VERSION = '2.10';  # <-- do not forget to update the POD section just above this line!
+our $VERSION = '2.12';  # <-- do not forget to update the POD section just above this line!
 $VERSION = eval $VERSION;
 
 =pod
